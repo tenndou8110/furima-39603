0b018f1af625f122c7327fc33cc2629ea4396763
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-21 17:04:22 +0900

    Revert "Revert "Revert "Revert "ユーザー管理機能の実装""""
    This reverts commit b44452f94165fc6912bb99858c7b61f0df0831a5.

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {'ニックネーム'}
+    email     {'test@test'}
+    password {'pass111'}
+    password_confirmation {'pass111'}
+    date_of_birth {'2002-07-16'}
+    firast_name {'名'}
+    last_name {'姓'}
+    firast_name_kana {'メイ'}
+    last_name_kana {'セイ'}
+  end
+end
