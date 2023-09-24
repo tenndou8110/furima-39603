88d62b853d0366c3a5a54e0bf6788de7d8ec4eff
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-24 17:21:55 +0900

    Revert "Revert "Revert "Revert "修正""""
    This reverts commit 51e9d5f9e5fa523aa41f2059d7b49233e116f5ca.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password              {'pass111'}
     password_confirmation {'pass111'}
     date_of_birth         {'2002-07-16'}
-    firast_name           {'名'}
+    first_name           {'名'}
     last_name             {'姓'}
-    firast_name_kana      {'メイ'}
+    first_name_kana      {'メイ'}
     last_name_kana        {'セイ'}
   end
 end
